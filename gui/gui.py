327c30712c0d69bd71b58023ad5b96b09fc8f7d8
maroon-scorch <matthew_ji@brown.edu>
2022-10-19 19:54:21 -0400

    Refined GUI + File Export

--- gui/gui.py
+++ gui/gui.py
@@ -1,9 +1,11 @@
 
 from tkinter import *
 from tkinter import ttk
+import os
+
 sys.path.insert(1, '../code')
 from point import Point
-from main import run
+from main import run, closest_grid_point
 
 win=Tk()
 
@@ -11,11 +13,14 @@ point_list = []
 
 screen_width = 400
 screen_height = 400
+ratio = 20
 
 # Setup of the window
 win.geometry("400x500")
 win.resizable(False, False)
 win.title('GUI')
+
+prev_point = [None, None]
 
 def draw_line(event):
    x1=event.x
@@ -26,9 +31,14 @@ def draw_line(event):
    # Draw an oval in the given co-ordinates
    canvas.create_oval(x1,y1,x2,y2,fill="black", width=10)
    
+   if prev_point != [None, None]:
+       canvas.create_line(prev_point[0], prev_point[1], x1, y1, fill="black", width=5)
+
+   prev_point[0] = x1;
+   prev_point[1] = y1;
    cartX = x1 - screen_width/2
    cartY = screen_height/2 - y1
-   cartPoint = Point(int(cartX), int(cartY))
+   cartPoint = closest_grid_point(Point(cartX/ratio, cartY/ratio))
    
    print(cartPoint)
    
@@ -45,6 +55,17 @@ def clickClearButton():
     
 def clickRunButton():
     print(point_list)
+    
+    filename = "output.txt"
+    file = open(filename,'wt')
+    file.write("2\n")
+    
+    for point in point_list:
+        px = point.x
+        py = point.y
+        line = str(px) + " 1 " + str(py) + " 1\n"
+        file.write(line)
+    
     win.destroy()
     run(point_list, 2)
 
