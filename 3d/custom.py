ef23641f3b4896c0365557ed5aec62d6b12d6f8d
maroon-scorch <matthew_ji@brown.edu>
2022-11-02 14:30:49 -0400

    Added Customized Curve and Fixed Some Bugs in Curve

---
+++ 3d/custom.py
@@ -0,0 +1,64 @@
+from point3 import *
+from main import run
+import math
+
+def custom_cuve(curve_x, curve_y, curve_z, start, stop, num_points, scale):
+    # Given a parameterized function for a curve, produces its polygonal approximation
+    samples = np.linspace(start, stop, num=num_points).tolist()
+    print(samples)
+    
+    points = []
+    for t in samples:
+        current_x = round(scale*curve_x(t))
+        current_y = round(scale*curve_y(t))
+        current_z = round(scale*curve_z(t))
+        # current_x = scale*curve_x(t)
+        # current_y = scale*curve_y(t)
+        # current_z = scale*curve_z(t)
+        points.append(Point3(current_x, current_y, current_z))
+        
+    return points
+
+# Broken Circle
+f_x = lambda t: math.cos(t)
+f_y = lambda t: 0.0001
+f_z = lambda t: math.sin(t)
+# points = custom_cuve(f_x, f_y, f_z, 0, 2*math.pi , 20, 30)
+# Might have issue at close point?
+points = custom_cuve(f_x, f_y, f_z, 1, 2*math.pi + 1 , 20, 30)
+
+# Un-knot
+theta = math.pi/4
+f_x = lambda t: math.cos(t)
+f_y = lambda t: 0.001
+f_z = lambda t: math.sin(t)
+t_x = lambda t: f_x(t)*math.cos(theta) - f_z(t)*math.sin(theta)
+t_z = lambda t: f_x(t)*math.sin(theta) + f_z(t)*math.cos(theta)
+points = custom_cuve(t_x, f_y, t_z, 0, 2*math.pi , 20, 30)
+
+# Trefoil
+f_x = lambda t: math.sin(t) + 2*math.sin(2*t)
+f_y = lambda t: math.cos(t) - 2*math.cos(2*t)
+f_z = lambda t: -math.sin(3*t)
+points = custom_cuve(f_x, f_y, f_z, 0, 2*math.pi , 20, 30)
+
+# Spiral
+f_x = lambda t: math.cos(t)
+f_y = lambda t: math.sin(t)
+f_z = lambda t: t
+points = custom_cuve(f_x, f_y, f_z, 0, 8*math.pi , 20, 30)
+
+# Twisted Cubic
+f_x = lambda t: t
+f_y = lambda t: t*t
+f_z = lambda t: t*t*t
+points = custom_cuve(f_x, f_y, f_z, -3, 3 , 20, 30)
+
+# theta = math.pi/4
+# t_x = lambda t: f_x(t)*math.cos(theta) - f_y(t)*math.sin(theta)
+# t_y = lambda t: f_x(t)*math.sin(theta) + f_y(t)*math.cos(theta)
+
+# Avoid Catastrophy
+# f_y = lambda t: 0.1
+# points = custom_cuve(f_x, f_y, f_z, 0, 2*math.pi, 20, 30)
+run(points, 3, False)
--- 3d/main.py
+++ 3d/main.py
@@ -7,9 +7,11 @@ from mpl_toolkits.mplot3d import Axes3D
 from functools import reduce
 import math
 
-epsilon = 0.1
+epsilon = 0.01
+delta = 0.00000000001
 min_range = 160/180*math.pi
 max_range = 200/180*math.pi
+max_iter = 10
 
 # Run program using 'python main.py [directory to file]'
 def read_input(inputFile):
@@ -45,7 +47,6 @@ def bad_vertices(points):
     for triple in ang_list:
         ang = angle(triple[0], triple[1], triple[2])
         if not (min_range <= ang and ang <= max_range):
-            print(ang)
             faulty_ver_list.append(triple[1].vec.tolist())
     
     return faulty_ver_list
@@ -62,7 +63,7 @@ def validate_input(points):
     for pt in points:
         if not isinstance(pt.x, int) and not isinstance(pt.y, int) and not isinstance(pt.z, int):
             print("Error: " + str(pt) + " does not have integer coordinate")
-            sys.exit(0)
+            # sys.exit(0)
             
     # Angle of any three consecutive points are 160-200 degree
     faulty_ver_list = bad_vertices(points)
@@ -91,7 +92,14 @@ def index_segment(point_1, point_2):
     that meets the integer lattice """
     # If they are the same point, this is degenerate line
     if point_1 == point_2:
-        return [point_1]
+        pt = point_1
+        is_x_int = abs(round(pt.x) - pt.x) < delta
+        is_y_int = abs(round(pt.y) - pt.y) < delta
+        is_z_int = abs(round(pt.z) - pt.z) < delta
+        if is_x_int or is_y_int or is_z_int:
+            return [pt]
+        else:
+            return []
     
     x_start = point_1.x
     y_start = point_1.y
@@ -102,7 +110,7 @@ def index_segment(point_1, point_2):
     
     x_int = int_between(x_start, x_end) # List of integers the x-value passes through
     y_int = int_between(y_start, y_end) # List of integers the y-value passes through
-    z_int = int_between(z_start, z_end) # List of integers the y-value passes through
+    z_int = int_between(z_start, z_end) # List of integers the z-value passes through
     
     x_int_cord = []
     y_int_cord = []
@@ -114,19 +122,19 @@ def index_segment(point_1, point_2):
         for x_cord in x_int:
             t = (x_cord - x_start)/diff[0]
             target = point_1.vec + t*diff
-            target_point = Point3(target[0], target[1], target[2])
+            target_point = Point3(x_cord, target[1], target[2])
             x_int_cord.append(target_point)
     if y_start != y_end:
         for y_cord in y_int:
             t = (y_cord - y_start)/diff[1]
             target = point_1.vec + t*diff
-            target_point = Point3(target[0], target[1], target[2])
+            target_point = Point3(target[0], y_cord, target[2])
             y_int_cord.append(target_point)
     if z_start != z_end:
         for z_cord in z_int:
             t = (z_cord - z_start)/diff[2]
             target = point_1.vec + t*diff
-            target_point = Point3(target[0], target[1], target[2])
+            target_point = Point3(target[0], target[1], z_cord)
             z_int_cord.append(target_point)
 
     # Only want unique results
@@ -140,9 +148,9 @@ def index_segment(point_1, point_2):
 def avoid_grid(points):
     result = []
     for pt in points:
-        is_x_int = abs(int(pt.x) - pt.x) < 0.00001
-        is_y_int = abs(int(pt.y) - pt.y) < 0.00001
-        is_z_int = abs(int(pt.z) - pt.z) < 0.00001
+        is_x_int = abs(round(pt.x) - pt.x) < delta
+        is_y_int = abs(round(pt.y) - pt.y) < delta
+        is_z_int = abs(round(pt.z) - pt.z) < delta
         if is_x_int and is_y_int and is_z_int:
             result.append(Point3(pt.x + epsilon, pt.y + epsilon, pt.z))
         elif is_x_int and is_y_int:
@@ -157,9 +165,9 @@ def avoid_grid(points):
 
 def has_grid_point(points):
     for pt in points:
-        is_x_int = abs(int(pt.x) - pt.x) < 0.00001
-        is_y_int = abs(int(pt.y) - pt.y) < 0.00001
-        is_z_int = abs(int(pt.z) - pt.z) < 0.00001
+        is_x_int = abs(round(pt.x) - pt.x) < delta
+        is_y_int = abs(round(pt.y) - pt.y) < delta
+        is_z_int = abs(round(pt.z) - pt.z) < delta
         if is_x_int and is_y_int and is_z_int:
             return True
         elif is_x_int and is_y_int:
@@ -178,6 +186,26 @@ def vert_to_edges_open(points):
             edges.append([point, points[idx + 1]])
     return edges
 
+def clean_input(points):
+    result = []
+    for pt in points:
+        is_x_int = abs(round(pt.x) - pt.x) < delta
+        is_y_int = abs(round(pt.y) - pt.y) < delta
+        is_z_int = abs(round(pt.z) - pt.z) < delta
+        
+        px, py, pz = pt.x, pt.y, pt.z
+        
+        if is_x_int:
+            px = int(round(pt.x))
+        if is_y_int:
+            py = int(round(pt.y))
+        if is_z_int:
+            pz = int(round(pt.z))
+        result.append(Point3(px, py, pz))
+
+    return result
+        
+
 def refine_input(points):
     # Step 1: Start from one of the end point, index each line segment cut by the grid
     adj_list = vert_to_edges_open(points)
@@ -187,22 +215,21 @@ def refine_input(points):
 
     # Avoid Grid Points by Perturbing it
     # refined_points = avoid_grid(refined_points)
+    refined_points = clean_input(refined_points)
     return refined_points
 
 
 def label(point):
     """ Given a point on the integer grid, labels whether it's on the vertical or horizontal part """
-    is_on_x = (point.x - int(point.x) == 0)
-    is_on_y = (point.y - int(point.y) == 0)
-    is_on_z = (point.z - int(point.z) == 0)
+    is_on_x = (point.x - round(point.x) == 0)
+    is_on_y = (point.y - round(point.y) == 0)
+    is_on_z = (point.z - round(point.z) == 0)
     return is_on_x, is_on_y, is_on_z
 
 def visualize(points, title, want_bad_vert):
     """ Given a list of points and a title, draws the curve traced out by it """
     input = map(lambda pt: [pt.x, pt.y, pt.z], points)
     x_pts, y_pts, z_pts= zip(*input) #create lists of x and y values
-    
-    print(z_pts)
         
     ax = plt.figure().add_subplot(projection='3d')
     ax.plot(x_pts, y_pts, z_pts, label='Polygonal Curve')
@@ -252,8 +279,8 @@ def solve_half_length(points):
         current_p2 = edge[1]
         
         # Code breaks on integer lattice lol
-        print(current_p1)
-        print(current_p2)
+        # print(current_p1)
+        # print(current_p2)
 
         mid_x_1 = (math.floor(current_p1.x) + math.ceil(current_p1.x))/2
         mid_y_1 = (math.floor(current_p1.y) + math.ceil(current_p1.y))/2
@@ -293,7 +320,7 @@ def solve_half_length(points):
     # for i, ed in enumerate(grid_edge_list):
     #     print(str(i) + ": " + str(ed))
 
-    # visualize_edges(grid_edge_list)
+    visualize_edges(grid_edge_list)
     grid_list = list(map(lambda ed: ed[0], grid_edge_list))
     grid_list.append(grid_edge_list[-1][1])
     
@@ -303,9 +330,10 @@ def solve_half_length(points):
 #               Main Operation
 # --------------------------------------------------------
 def run(points, dimension, close):
+    points = clean_input(points)
     validate_input(points)
-    # if close and points[0] != points[-1]:
-    #     points.append(points[0])
+    if close and points[0] != points[-1]:
+        points.append(points[0])
         
     print("This is a curve in dimension: ", dimension)
     print("The list has length: ", len(points))
@@ -314,15 +342,18 @@ def run(points, dimension, close):
     visualize(points, "Raw Input", False)
     # Refine the inputs first
     refined_points = refine_input(points)
-    while(has_grid_point(refined_points)):
+    i = 0
+    while(has_grid_point(refined_points) and i < max_iter):
         refined_points = avoid_grid(refined_points)
         refined_points = refine_input(refined_points)
+        i = i + 1
 
     print("Has Grid Points: ", has_grid_point(refined_points))
     visualize(refined_points, "Refined Input", False)
+    print("Refined Points: ", refined_points)
     solution = solve_half_length(refined_points)
     print(solution)
-    visualize(solution, "Grid Curve", False)
+    # visualize(solution, "Grid Curve", False)
 
 # The main body of the code:
 if __name__ == "__main__":
