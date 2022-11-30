acc9f124409beb027d6726994c3fcca9166ac2c7
maroon-scorch <matthew_ji@brown.edu>
2022-11-30 18:54:41 -0500

    Fixed Bug + Added Specific Curves

--- code/calc.py
+++ code/calc.py
@@ -112,19 +112,52 @@ def rotate(f_x, f_y, theta):
     t_x = lambda t: f_x(t)*math.cos(theta) - f_y(t)*math.sin(theta)
     t_y = lambda t: f_x(t)*math.sin(theta) + f_y(t)*math.cos(theta)
     return t_x, t_y
+
+def rotate_curve(points, index):
+    points_list = []
+    
+    if index == 1:
+        for pt in points: 
+            t_x =  -pt.y
+            t_y = pt.x
+            points_list.append(Point(t_x, t_y))
+    elif index == 2:
+        for pt in points: 
+            t_x =  -pt.x
+            t_y = -pt.y
+            points_list.append(Point(t_x, t_y))
+    elif index == 3:
+        for pt in points: 
+            t_x =  pt.y
+            t_y = -pt.x
+            points_list.append(Point(t_x, t_y))
+    else:
+        points_list = points        
+    return points_list
     
 # points = generate_curve(-10, 10, 10, 2)
 # # points = getpts(-10, 10, 3)
 # visualize(points, "Title", True)
-if __name__ == "__main__":
-    
+
+# Return a list of specific grid curves to test for
+def specific_curve():
+    c0 = [Point(0, 0), Point(1, 0), Point(2, 0), Point(2, 1), Point(1, 1), Point(1, 2), Point(0, 2), Point(0, 1)]
+    curves = [c0]
+    output = []
+    for c in curves:
+        c1, c2, c3 = rotate_curve(c, 1), rotate_curve(c, 2), rotate_curve(c, 3)
+        output += [c, c1, c2, c3]
+    return output
+
+if __name__ == "__main__": 
     polynomial_list = []
     parameter_list = []
     grid_curve_list = []
+    
     for i in range(10):
         a = random.randint(-5, -1)
         b = random.randint(1, 5)
-        f_x = lambda t: math.cos(a*t)
+        f_x = lambda t: math.cos(a*t) + 1
         f_y = lambda t: math.sin(b*t)
         f1_x, f1_y = rotate(f_x, f_y, math.pi/2)
         f2_x, f2_y = rotate(f_x, f_y, math.pi)
@@ -136,12 +169,14 @@ if __name__ == "__main__":
             parameter_list.append(param)
             points = custom_cuve(x, y, 0, 2*math.pi, 100, 30)
             solution = run(points, 2, False)
-            grid_curve_list.append(solution)
-        
-        for curve in grid_curve_list:
-            count_list = process_solution(curve)
-            polynomial_list.append([count_list, curve])
-
+            # grid_curve_list.append(solution)
+    
+    grid_curve_list += specific_curve()
+     
+    for curve in grid_curve_list:
+        count_list = process_solution(curve)
+        polynomial_list.append([count_list, curve])
+    print(len(polynomial_list))
     print("-------------------------------------------------------------")
     with open('poly.txt', 'w') as f:
         for c_lst, solution in polynomial_list:
