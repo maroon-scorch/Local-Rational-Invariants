52ecd882825d336509d7128fc04a1c57f7f0c9d7
maroon-scorch <matthew_ji@brown.edu>
2022-12-29 02:56:14 -0500

    Appeal to Infinity

--- 3d/visualize_index.py
+++ 3d/visualize_index.py
@@ -28,20 +28,17 @@ def index_to_squares(order_list):
     return sq_list
 
 if __name__ == "__main__":
-    # order_list = [
-    #     [2,3,5,4,6,2],
-    #     [1,3,6,1],
-    #     [2,4,5,2],
-    #     [1,4,6,3,5,1]
-    # ]
+    order_list = [
+        [1,3,6,4,1],
+    ]
     
-    # for i in range(len(order_list)):
-    #     print("Vertex ", i)
-    #     sq_list = index_to_squares(order_list[i])
-    #     sq_list = integerify_square(rotate_squares_psi(sq_list, math.pi/2))
+    for i in range(len(order_list)):
+        print("Vertex ", i)
+        sq_list = index_to_squares(order_list[i])
+        # sq_list = integerify_square(rotate_squares_psi(sq_list, 2*math.pi/2))
 
-    #     print(vert_link(Point3(0, 0, 0), sq_list))
-    #     square_to_voxel(sq_list)
+        print(vert_link(Point3(0, 0, 0), sq_list))
+        square_to_voxel(sq_list)
     
     # order_list = [
     #     [1,4,6,2,3,5,1],
@@ -55,40 +52,42 @@ if __name__ == "__main__":
     #     [1,4,5,1]
     # ]
     
-    order_list = [
-        "14261",
-        "14251",
-        "13641",
-        "25362",
-        "235462",
-        "145361",
-        "146351",
-        "2452",
-        "136451",
-        "2462",
-        "1362541",
-        "25462",
-        "132541",
-        "1463251",
-        "132641"
-    ]
+    # order_list = [
+    #     "14261",
+    #     "14251",
+    #     "13641",
+    #     "25362",
+    #     "235462",
+    #     "145361",
+    #     "146351",
+    #     "2452",
+    #     "136451",
+    #     "2462",
+    #     "1362541",
+    #     "25462",
+    #     "132541",
+    #     "1463251",
+    #     "132641"
+    # ]
     
-    order_list.sort(key = lambda x: len(x))
-    print(len(order_list))
-    
-    new_list = []
-    for elt in order_list:
-        input = []
-        for c in elt:
-            input.append(int(c))
-        new_list.append(input)
-    
-    print(new_list)
-    
-    for i in range(len(new_list)):
-        print("Vertex ", i)
-        sq_list = index_to_squares(new_list[i])
-        # sq_list = integerify_square(rotate_squares_theta(sq_list, 3*math.pi/2))
 
-        print(vert_link(Point3(0, 0, 0), sq_list))
-        square_to_voxel(sq_list)
+    
+    # order_list.sort(key = lambda x: len(x))
+    # print(len(order_list))
+    
+    # new_list = []
+    # for elt in order_list:
+    #     input = []
+    #     for c in elt:
+    #         input.append(int(c))
+    #     new_list.append(input)
+    
+    # print(new_list)
+    
+    # for i in range(len(new_list)):
+    #     print("Vertex ", i)
+    #     sq_list = index_to_squares(new_list[i])
+    #     # sq_list = integerify_square(rotate_squares_theta(sq_list, 3*math.pi/2))
+
+    #     print(vert_link(Point3(0, 0, 0), sq_list))
+    #     square_to_voxel(sq_list)
